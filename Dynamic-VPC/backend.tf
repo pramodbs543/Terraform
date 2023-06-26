75197d39893ebd2d9f9981356f1021ef41bf4e96
pramodbs543 <130469998+pramodbs543@users.noreply.github.com>
2023-06-26 11:20:02 +0530

    Update backend.tf

--- Dynamic-VPC/backend.tf
+++ Dynamic-VPC/backend.tf
@@ -1,9 +1,9 @@
 terraform {
 backend "s3" {
-    bucket         = "terraformbucket9049"  # Replace with your S3 bucket name
+    bucket         = "<bucket_name>"  # Replace with your S3 bucket name
     key            = "terraform.tfstate"
     region         = "ap-south-1"  # Replace with your desired region
-    dynamodb_table = "tf-backend-s3-demo"  # Replace with your DynamoDB table name
+    dynamodb_table = "<Dynamo db table name>"  # Replace with your DynamoDB table name
     encrypt        = true  # Optional: Enable encryption for the state file
   }
 
