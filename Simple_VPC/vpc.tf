9ab3bf176646b3fc39d50e3ee524b21f6949358c
pramodbs543 <130469998+pramodbs543@users.noreply.github.com>
2023-07-23 22:14:06 +0530

    Update vpc.tf

--- Simple_VPC/vpc.tf
+++ Simple_VPC/vpc.tf
@@ -69,9 +69,9 @@ resource "aws_route_table" "private_rt" {
   }
 }
 resource "aws_route" "private" {
-  route_table_id         = "aws_route_table.private_rt.id"
+  route_table_id         = aws_route_table.private_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_nat_gateway.gw.id}"
+  gateway_id             = aws_nat_gateway.gw.id
 }
 
 resource "aws_route_table_association" "private-association" {
