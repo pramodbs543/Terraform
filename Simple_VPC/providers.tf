6c76c1be8284b09aa6512d119b29fb0a5efc1902
pramodbs543 <130469998+pramodbs543@users.noreply.github.com>
2023-07-26 10:22:14 +0530

    Update providers.tf

--- Simple_VPC/providers.tf
+++ Simple_VPC/providers.tf
@@ -11,11 +11,12 @@ terraform {
 provider "aws" {
   region     = var.region
   #The access and secret keys are stored as part of environment variables.
-  #This is for windows powershell.
+  
+  #For terraform in windows powershell.
   # $env:AWS_ACCESS_KEY_ID = "YOUR_ACCESS_KEY"
   # $env:AWS_SECRET_ACCESS_KEY = "YOUR_SECRET_KEY"
   
-  # For Linux:
+  # For terraform running in linux
   # export AWS_ACCESS_KEY_ID=Your_Access_Key 
   # export AWS_SECRET_ACCESS_KEY=Your_Secred_Access_Key 
 
